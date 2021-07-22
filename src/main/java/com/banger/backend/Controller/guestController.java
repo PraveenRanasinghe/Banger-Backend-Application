4a707b2dd80d487777b78c703fd81bebebf8d379
PraveenRanasinghe <praveenranasinghe43@gmail.com>
2021-07-23 01:13:18 +0530

    Finalized rolebased login

--- src/main/java/com/banger/backend/Controller/guestController.java
+++ src/main/java/com/banger/backend/Controller/guestController.java
@@ -1,12 +1,17 @@
 package com.banger.backend.Controller;
 
 import com.banger.backend.DTO.inquiryDTO;
+import com.banger.backend.DTO.vehicleDTO;
 import com.banger.backend.Entity.Inquiry;
+import com.banger.backend.Entity.Vehicle;
 import com.banger.backend.Service.inquiryService;
+import com.banger.backend.Service.vehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -17,11 +22,19 @@ public class guestController {
     @Autowired
     private inquiryService inqService;
 
+    @Autowired
+    private vehicleService vehicleService;
+
     @PostMapping("/guestAddInq")
     @PreAuthorize("permitAll()")
     public ResponseEntity<inquiryDTO> guestAddInquiry(@RequestBody inquiryDTO dto){
         Inquiry addInq= this.inqService.addInquiry(dto);
         return new ResponseEntity(addInq,OK);
+    }
+
+    @GetMapping("/guestViewVehicles")
+    public List<Vehicle> viewAllVehicles() {
+        return vehicleService.getAllVehicles();
     }
 
 }
