0369aadedc98e7a5d24aa6aa5119e974e8e30894
PraveenRanasinghe <praveenranasinghe43@gmail.com>
2021-08-04 15:42:28 +0530

    working on ui improvements

--- src/main/java/com/banger/backend/Service/bookingService.java
+++ src/main/java/com/banger/backend/Service/bookingService.java
@@ -73,7 +73,7 @@ public class bookingService {
                 dto.setBookingId(bookings.getBookingId());
                 dto.setReturnTime(bookings.getReturnTime().toString());
                 dto.setPickupTime(bookings.getPickupTime().toString());
-                dto.setEquipments(bookings.getEquipments());
+//                dto.setEquipments(bookings.getEquipments());
                 dto.setVehicle(bookings.getVehicle());
                 dto.setBookingStatus(bookings.getBookingStatus());
 
